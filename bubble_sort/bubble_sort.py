32ec3cced29036a5a95472fc14545806f826166b
JasonWachira <jason.kamwaro@strathmore.edu>
2025-06-20 12:05:07 +0300

    Did selection sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -2,7 +2,8 @@ def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
     n = len(unsorted_list)
     for i in range(n-1):
-        # We don't need to compare with the already sorted elements at the end of the array so the inner loop runs less i times
+        # We don't need to compare with the already sorted elements at the end of the array
+        # so the inner loop runs less i times
         for j in range(n-i-1):
             if unsorted_list[j] > unsorted_list[j+1]:
                 unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,14 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    for i in range(n-1):
+        min_elem = arr[i]
+        for j in range(i+1, n):
+            if arr[j] < arr[i]:
+                min_elem = arr[j]
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+
+unsorted = [9,8,5,4,3]
+print(selection_sort(unsorted))
